aafcd9d4d0bc269dfd66487e1ebb2138a0f07eb5
michaelmccrae <mike@mccrae.ca>
2025-04-28 11:54:01 -0700

    No script

--- main/app/layout.tsx
+++ main/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter, Nokora } from "next/font/google";
 import "./globals.css";
 import Testimonial from './components/testimonials'
-import Script from "next/script" // for reCaptcha
+// import Script from "next/script" // for reCaptcha
 
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
