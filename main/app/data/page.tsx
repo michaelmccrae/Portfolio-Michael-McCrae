146bb3bd7eaa1cc4ff1eed49751aad63d2a42fcf
michaelmccrae <mike@mccrae.ca>
2025-05-07 19:27:52 -0700

    Updated data page

--- main/app/data/page.tsx
+++ main/app/data/page.tsx
@@ -29,7 +29,9 @@ const page = () => {
               </div> 
 
               
-<p>I have strong data analysis skills. At MINING.com, I modeled data from Mining Intelligence using the tidyverse and RStudio to explore and create data-driven stories. The MINING.com team was a finalist in the Jack Webster Awards</p>
+<p>I have strong data analysis skills. At MINING.com, I modeled data from the business's sister division, Mining Intelligence. I used R to explore datasets and craft stories. In 2014, one of the MINING.com editors was a Jack Webster Awards finalist in the data journalism category.</p>
+
+<p>At Kitco Mining, I used JavaScript and D3 to make slope graphs, as well as <Link href="https://x.com/michaelmccrae/status/1105903869744209920">animated charts for social</Link>.</p>
 
 <figure className="">
 <Image
@@ -39,8 +41,10 @@ height={0}
 sizes="100vw"
 className="w-full h-auto"
 alt="A ridgeline plot by Michael McCrae showing pay range by job title at Canadian mines"/>
-<figcaption className="caption">A ridgeline plot developed by Michael McCrae for Mining Intelligence, showing <Link href="https://www.linkedin.com/pulse/top-paid-positions-canadian-mines-michael-allan-mccrae-mba-cma/?trackingId=gQex2CCdQUuo7u3Xmwiw8A%3D%3D">pay range by job title at Canadian mines</Link>. Data was compiled in 2017.</figcaption>
+<p>A ridgeline plot developed by Michael McCrae for Mining Intelligence, showing <Link href="https://www.linkedin.com/pulse/top-paid-positions-canadian-mines-michael-allan-mccrae-mba-cma/?trackingId=gQex2CCdQUuo7u3Xmwiw8A%3D%3D">pay range by job title at Canadian mines</Link>. Data was compiled in 2017.</p>
 </figure>
+
+<hr className="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700"></hr>
 
 <figure className="">
 <Image
@@ -50,8 +54,26 @@ height={0}
 sizes="100vw"
 className="w-full h-auto"
 alt="A sankey diagram by Michael McCrae showing resource financings in October 2017" />
-<figcaption className="caption">A sankey diagram developed by Michael McCrae for Mining Intelligence showing a breakdown of <Link href="https://www.linkedin.com/pulse/lithium-companies-raise-us42-million-october-mccrae-mba-cma/?trackingId=gQex2CCdQUuo7u3Xmwiw8A%3D%3D">financings by resource companies in October 2017</Link>.</figcaption>
-</figure>    
+<p>A sankey diagram developed by Michael McCrae for Mining Intelligence showing a breakdown of <Link href="https://www.linkedin.com/pulse/lithium-companies-raise-us42-million-october-mccrae-mba-cma/?trackingId=gQex2CCdQUuo7u3Xmwiw8A%3D%3D">financings by resource companies in October 2017</Link>.</p>
+</figure>  
+
+<hr className="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700"></hr>
+
+<figure className="">
+<Image
+src="/topGoldSlope.jpg"
+width={0}
+height={0}
+sizes="100vw"
+className="w-full h-auto"
+alt="The world’s top 10 largest gold mining companies based on the miner's production figures reported for 2022 (calendar year)."/>
+<p>The world’s <Link href="https://observablehq.com/d/3a6ac5c138eb76ed">top 10 largest gold mining companies</Link> based on the miner's production figures reported for 2022 (calendar year). </p>
+</figure>
+
+<hr className="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700"></hr>
+
+
+
     </div>
     </>
   )
